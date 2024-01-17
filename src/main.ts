885023565eb7e4b7fd4d1e0b601f9343084b5a4f
Eduardo Fernandes <eduardo.yugan@gmail.com>
2024-01-17 10:32:27 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NextFunction, Request, Response } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.use((req: Request, res: Response, next: NextFunction, error: Error) => {
-    console.log('deu erro', error);
-
-    next();
-  });
 
   await app.listen(3000);
 }
